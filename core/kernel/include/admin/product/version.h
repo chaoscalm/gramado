ce43534182681f65282c019298cef49e504a6c3d
frednora <freddynora@gmail.com>
2023-09-04 18:48:58 -0300

    Gramado OS: Build 321.

--- core/kernel/include/admin/product/version.h
+++ core/kernel/include/admin/product/version.h
@@ -9,7 +9,7 @@
 
 // Current build number.
 // Updated by the builder.
-#define __CURRENT_BUILD_NUMBER    320
+#define __CURRENT_BUILD_NUMBER    321
 
 // Version
 #define VERSION_MAJOR    1
@@ -19,10 +19,11 @@
 #define VERSION_WORD    0x0108
 // Strings
 #define VERSION_STRING    "1.8"
-#define BUILD_STRING      "320"
+#define BUILD_STRING      "321"
 #define RELEASE_STRING    PRODUCT_TYPE_STRING
 
 #endif
+
 
 
 
