0eb7fbe3b10e28e9a75da854389f56acd8ad910d
frednora <freddynora@gmail.com>
2023-09-10 15:21:59 -0300

    Gramado OS: Build 325.

--- ama/kernel/include/admin/product/version.h
+++ ama/kernel/include/admin/product/version.h
@@ -9,7 +9,7 @@
 
 // Current build number.
 // Updated by the builder.
-#define __CURRENT_BUILD_NUMBER    324
+#define __CURRENT_BUILD_NUMBER    325
 
 // Version
 #define VERSION_MAJOR    1
@@ -19,7 +19,7 @@
 #define VERSION_WORD    0x0108
 // Strings
 #define VERSION_STRING    "1.8"
-#define BUILD_STRING      "324"
+#define BUILD_STRING      "325"
 #define BUILD_CODENAME_STRING  "Amazing"
 #define RELEASE_STRING    PRODUCT_TYPE_STRING
 
