1dd85e1afb46c7d917b9c1b3c878cecd4c133b47
frednora <freddynora@gmail.com>
2023-09-20 21:30:19 -0300

    Gramado OS: Build 331

--- ama/kernel/admin/include/admin/product/version.h
+++ ama/kernel/admin/include/admin/product/version.h
@@ -9,7 +9,7 @@
 
 // Current build number
 // Updated by the builder
-#define __CURRENT_BUILD_NUMBER    330
+#define __CURRENT_BUILD_NUMBER    331
 
 // Version
 #define VERSION_MAJOR    1
@@ -19,7 +19,7 @@
 #define VERSION_WORD    0x0101
 // Strings
 #define VERSION_STRING    "1.1"
-#define BUILD_STRING      "330"
+#define BUILD_STRING      "331"
 #define BUILD_CODENAME_STRING  "Amazing"
 #define RELEASE_STRING    PRODUCT_TYPE_STRING
 
