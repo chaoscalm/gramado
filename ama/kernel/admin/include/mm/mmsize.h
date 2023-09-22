e038612d3066d14aa8b9d531eee0f46da37eccb5
frednora <freddynora@gmail.com>
2023-09-21 21:31:28 -0300

    mm: system size.

--- ama/kernel/admin/include/mm/mmsize.h
+++ ama/kernel/admin/include/mm/mmsize.h
@@ -11,27 +11,31 @@
 #define GB  (1024 * 1024 * 1024)
 
 
+#define __SMALL   ( 256 -1)
+#define __MEDIUM  ( 512 -1)
+#define __LARGE   (1024 -1)
+
 // --------------
 // Tamanho dado em bytes.
-#define SMALLSYSTEM_SIZE  ( 256*1024*1024)
-#define MEDIUMSYSTEM_SIZE ( 512*1024*1024)
-#define LARGESYSTEM_SIZE  ( 1024*1024*1024)
+#define SMALLSYSTEM_SIZE  ( __SMALL  *1024*1024)
+#define MEDIUMSYSTEM_SIZE ( __MEDIUM *1024*1024)
+#define LARGESYSTEM_SIZE  ( __LARGE  *1024*1024)
 
 // Tamanho do sistema, dado em KB.
-#define SMALLSYSTEM_SIZE_KB  ( 256*1024)
-#define MEDIUMSYSTEM_SIZE_KB ( 512*1024)
-#define LARGESYSTEM_SIZE_KB  (1024*1024)
+#define SMALLSYSTEM_SIZE_KB  (__SMALL  * 1024)
+#define MEDIUMSYSTEM_SIZE_KB (__MEDIUM * 1024)
+#define LARGESYSTEM_SIZE_KB  (__LARGE  * 1024)
 
 // #todo
 // Tamanho do sistema, dado em MB.
-#define SMALLSYSTEM_SIZE_MB  ( 256)
-#define MEDIUMSYSTEM_SIZE_MB ( 512)
-#define LARGESYSTEM_SIZE_MB  (1024)
+#define SMALLSYSTEM_SIZE_MB  (__SMALL)
+#define MEDIUMSYSTEM_SIZE_MB (__MEDIUM)
+#define LARGESYSTEM_SIZE_MB  (__LARGE)
 
 // Tamanho so sitema, dado em quantidade de páginas de 4KB.
-#define SMALLSYSTEM_SIZE_PAGES  ( ( 256*1024*1024) / 4096 )
-#define MEDIUMSYSTEM_SIZE_PAGES ( ( 512*1024*1024) / 4096 )
-#define LARGESYSTEM_SIZE_PAGES  ( (1024*1024*1024) / 4096 )
+#define SMALLSYSTEM_SIZE_PAGES  ( (__SMALL  *1024*1024) / 4096 )
+#define MEDIUMSYSTEM_SIZE_PAGES ( (__MEDIUM *1024*1024) / 4096 )
+#define LARGESYSTEM_SIZE_PAGES  ( (__LARGE  *1024*1024) / 4096 )
 // --------------
 
 
