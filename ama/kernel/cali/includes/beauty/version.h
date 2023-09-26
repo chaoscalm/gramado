e4e2033805e7386c6eea7ef2b847ecb60b95d523
frednora <freddynora@gmail.com>
2023-09-26 20:26:18 -0300

    Gramado OS: Build 334.

--- ama/kernel/cali/includes/beauty/version.h
+++ ama/kernel/cali/includes/beauty/version.h
@@ -9,17 +9,18 @@
 
 // Current build number
 // Updated by the builder
-#define __CURRENT_BUILD_NUMBER    333
+#define __CURRENT_BUILD_NUMBER    334
 
 // Version
 #define VERSION_MAJOR    1
 #define VERSION_MINOR    1
 #define VERSION_BUILD    __CURRENT_BUILD_NUMBER
 // Version word
+// Two bytes.
 #define VERSION_WORD    0x0101
 // Strings
 #define VERSION_STRING    "1.1"
-#define BUILD_STRING      "333"
+#define BUILD_STRING      "334"
 #define BUILD_CODENAME_STRING  "Amazing"
 #define RELEASE_STRING    PRODUCT_TYPE_STRING
 
