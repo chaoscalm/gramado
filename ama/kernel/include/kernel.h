26d6d55de8f7b47f31445494586b5ce5c716dfd4
frednora <freddynora@gmail.com>
2023-09-27 21:01:37 -0300

    kernel: Put all the headers together into a single folder.

--- ama/kernel/include/kernel.h
+++ ama/kernel/include/kernel.h
@@ -37,17 +37,17 @@ Hallelujah, my King - (Transfiguration)
 #include "beauty/config.h"
 
 // ==================================
-#include "k4/bootinfo.h"
-#include "k4/mode.h"
-#include "k4/state.h"
+#include "bootinfo.h"
+#include "mode.h"
+#include "state.h"
 // ==================================
 // utsname
 // Structure describing the system and machine
-#include "k4/u.h"          // User
-#include "k4/utsname.h"
+#include "u.h"          // User
+#include "utsname.h"
 // ==================================
 // system
-#include "k4/system.h"
+#include "system.h"
 // ==================================
 
 //
@@ -59,29 +59,29 @@ Hallelujah, my King - (Transfiguration)
 // Kernel configuration.
 // Compiling.
 //#include "kconfig.h"
-#include "k4/klimits.h"
-#include "k4/gdef.h"
+#include "klimits2.h"
+#include "gdef.h"
 
-#include "k3/gdevice.h"
-#include "k3/ginput.h"  // input manager support.
+#include "gdevice.h"
+#include "ginput.h"  // input manager support.
 
-#include "k2/gspin.h"
-#include "k2/gwd.h"     // whatch dogs.
-#include "k2/pints.h"   // profiler
+#include "gspin.h"
+#include "gwd.h"     // whatch dogs.
+#include "pints.h"   // profiler
 
-#include "k4/kinit.h"   // kernel initialization.
+#include "kinit.h"   // kernel initialization.
 
 // ===============================
 
-#include "k2/zcall/overall/info.h"    // last one?
-#include "k2/zcall/overall/request.h"
-#include "k2/zcall/overall/debug.h"
+#include "info.h"    // last one?
+#include "request.h"
+#include "debug.h"
 
 // Gramado configuration.
-#include "k2/cali/jiffies.h"
+#include "jiffies.h"
 
 // Setup input mode.
-#include "k3/dev/io_ctrl.h"
+#include "io_ctrl.h"
 
 
 //
@@ -89,86 +89,86 @@ Hallelujah, my King - (Transfiguration)
 //
 
 // crt: Libc support.
-#include "k2/public/ktypes.h"
-#include "k2/public/ktypes2.h"
+#include "ktypes.h"
+#include "ktypes2.h"
 
-#include "k2/cali/kobject.h"
+#include "kobject.h"
 
 // Legacy stuff.
-#include "k2/posix/kstdarg.h"
-#include "k2/posix/kerrno.h"
-#include "k2/posix/kcdefs.h"
-#include "k2/posix/kstddef.h"
-#include "k2/posix/klimits.h"
-#include "k2/posix/kstdio.h"
-#include "k2/posix/kstdlib.h"
-#include "k2/posix/kstring.h"
-#include "k2/posix/kctype.h"
-#include "k2/posix/kiso646.h"
-#include "k2/posix/ksignal.h"
-#include "k2/posix/kunistd.h"
-#include "k2/posix/kfcntl.h"
-#include "k2/posix/kioctl.h"
-#include "k2/posix/kioctls.h"
-#include "k2/posix/ktermios.h"
-#include "k2/posix/kttydef.h"
+#include "kstdarg.h"
+#include "kerrno.h"
+#include "kcdefs.h"
+#include "kstddef.h"
+#include "klimits.h"
+#include "kstdio.h"
+#include "kstdlib.h"
+#include "kstring.h"
+#include "kctype.h"
+#include "kiso646.h"
+#include "ksignal.h"
+#include "kunistd.h"
+#include "kfcntl.h"
+#include "kioctl.h"
+#include "kioctls.h"
+#include "ktermios.h"
+#include "kttydef.h"
 
 // Globals. PIDs support.
-#include "k2/cali/kpid.h"
+#include "kpid.h"
 
 //
 // + (4) Memory management.
 //
 
 // Memory management.
-#include "k1/mm/mmsize.h"
-#include "k1/mm/x64gpa.h"
-#include "k1/mm/x64gva.h"
-#include "k1/mm/gentry.h"
-#include "k1/mm/mm.h"
-#include "k1/mm/memmap.h" 
-#include "k1/mm/intelmm.h"
-#include "k1/mm/mmblock.h"
-#include "k1/mm/mmusage.h"
-#include "k1/mm/x64mm.h"
-#include "k1/mm/mmft.h"
-#include "k1/mm/mmglobal.h"  // Deve ficar mais acima.
-#include "k1/mm/heap.h"      // Heap pointer support.
-#include "k1/mm/aspace.h"    // Address Space, (data base account).
-#include "k1/mm/bank.h"      // Bank. database
+#include "mmsize.h"
+#include "x64gpa.h"
+#include "x64gva.h"
+#include "gentry.h"
+#include "mm.h"
+#include "memmap.h" 
+#include "intelmm.h"
+#include "mmblock.h"
+#include "mmusage.h"
+#include "x64mm.h"
+#include "mmft.h"
+#include "mmglobal.h"  // Deve ficar mais acima.
+#include "heap.h"      // Heap pointer support.
+#include "aspace.h"    // Address Space, (data base account).
+#include "bank.h"      // Bank. database
 
 //
 // + (5) HAL.
 //
 
 // hal
-#include "k2/hal/ports64.h"
-#include "k2/hal/cpu.h"
-#include "k2/hal/tss.h"
-#include "k2/hal/x64gdt.h"
-#include "k2/hal/x64.h"
-#include "k2/hal/detect.h"
+#include "ports64.h"
+#include "cpu.h"
+#include "tss.h"
+#include "x64gdt.h"
+#include "x64.h"
+#include "detect.h"
 // hal pci
-#include "k2/hal/bus/pci.h"  // PCI bus.
+#include "pci.h"  // PCI bus.
 // hv
-#include "k2/hal/hv.h"
+#include "hv.h"
 // hal cpu
-#include "k2/hal/cpuid.h"
-#include "k2/hal/up.h"
-#include "k2/hal/mp.h"
+#include "cpuid.h"
+#include "up.h"
+#include "mp.h"
 // hal pic/apic
-#include "k2/hal/pic.h"
-#include "k2/hal/apic.h"
-#include "k2/hal/apictim.h"
-#include "k2/hal/ioapic.h"
-#include "k2/hal/breaker.h"
+#include "pic.h"
+#include "apic.h"
+#include "apictim.h"
+#include "ioapic.h"
+#include "breaker.h"
 // hal timers.
-#include "k2/hal/pit.h"
-#include "k2/hal/rtc.h"
+#include "pit.h"
+#include "rtc.h"
 // hal io
-#include "k2/hal/io.h"    //io.
+#include "io.h"    //io.
 // hal global
-#include "k2/hal/hal.h"     // last one.
+#include "hal.h"     // last one.
 
 //
 // + (6) Devices.
@@ -181,11 +181,11 @@ Hallelujah, my King - (Transfiguration)
 //
 
 
-#include "k3/dev/tty/ttyldisc.h"
-#include "k3/dev/tty/ttydrv.h"
-#include "k3/dev/tty/tty.h"
-#include "k3/dev/tty/pty.h"
-#include "k3/dev/tty/console.h"
+#include "ttyldisc.h"
+#include "ttydrv.h"
+#include "tty.h"
+#include "pty.h"
+#include "console.h"
 
 //
 // Serial devices ------------------
@@ -193,49 +193,48 @@ Hallelujah, my King - (Transfiguration)
 
 // hw stuff - display device
 // display device support.
-#include "k3/dev/display/common/display.h"
+#include "display.h"
 // bootloader display device
-#include "k3/dev/display/bldisp/bldisp.h"
+#include "bldisp.h"
 
 // sw - Graphics Engine
-#include "k2/ke/gre/color.h"
-#include "k2/ke/gre/fonts.h"
-#include "k2/ke/gre/ascii.h"
-#include "k2/ke/gre/bg.h"
-#include "k2/ke/gre/ws2.h"
-#include "k2/ke/gre/gre.h"
+#include "color.h"
+#include "fonts.h"
+#include "ascii.h"
+#include "bg.h"
+#include "ws2.h"
+#include "gre.h"
 // caliuser
-#include "k2/ke/caliuser/ws.h"
-#include "k2/ke/caliuser/input.h"
-#include "k2/ke/caliuser/callback.h"
+#include "ws.h"
+#include "input.h"
+#include "callback.h"
 
 // Serial port. (COM).
-#include "k3/dev/tty/serial.h"
+#include "serial.h"
 
 // ps2 - i8042
-#include "k3/dev/kbd/vk.h"
-#include "k3/dev/kbd/kbdabnt2.h"
-#include "k3/dev/kbd/kbdmap.h"
-#include "k3/dev/kbd/keyboard.h"
-#include "k3/dev/kbd/ps2kbd.h"
-#include "k3/dev/mouse/mouse.h"
-#include "k3/dev/mouse/ps2mouse.h"
-#include "k3/dev/i8042.h"
-#include "k3/dev/ps2.h"
-
+#include "vk.h"
+#include "kbdabnt2.h"
+#include "kbdmap.h"
+#include "keyboard.h"
+#include "ps2kbd.h"
+#include "mouse.h"
+#include "ps2mouse.h"
+#include "i8042.h"
+#include "ps2.h"
 
 //
 // Block devices ----------------------
 //
 
 // ata, sata
-#include "k3/dev/ata/ata.h"
+#include "ata.h"
 
 // storage
-#include "k3/dev/superblk.h"
-#include "k3/dev/volume.h"
-#include "k3/dev/disk.h"  
-#include "k3/dev/storage.h" 
+#include "superblk.h"
+#include "volume.h"
+#include "disk.h"
+#include "storage.h"
 
 //
 // + (7) Network.
@@ -247,32 +246,34 @@ Hallelujah, my King - (Transfiguration)
 
 // primeiro controladoras depois protocolos
 // e1000 - nic intel
-#include "k3/dev/e1000/e1000.h"
+#include "e1000.h"
+
+
 // network
-#include "k2/net/mac.h"
-#include "k2/net/host.h"
-#include "k2/net/in.h"
-#include "k2/net/un.h"
+#include "net/mac.h"
+#include "net/host.h"
+#include "net/in.h"
+#include "net/un.h"
 // Protocols
-#include "k2/net/prot/ethernet.h"
-#include "k2/net/prot/arp.h"
-#include "k2/net/prot/ip.h"
-#include "k2/net/prot/tcp.h"
-#include "k2/net/prot/udp.h"
-#include "k2/net/prot/dhcp.h" 
-#include "k2/net/prot/icmp.h" 
+#include "net/ethernet.h"
+#include "net/arp.h"
+#include "net/ip.h"
+#include "net/tcp.h"
+#include "net/udp.h"
+#include "net/dhcp.h" 
+#include "net/icmp.h" 
 // Network
-#include "k2/net/nports.h"     //(network) Network Ports  (sw)
-#include "k2/net/inet.h"
-#include "k2/net/network.h"     //(network) Gerenciamento de rede.  
-#include "k2/net/socklib.h"     //
-#include "k2/net/socket.h"      //last always
+#include "net/nports.h"     //(network) Network Ports  (sw)
+#include "net/inet.h"
+#include "net/network.h"     //(network) Gerenciamento de rede.  
+#include "net/socklib.h"     //
+#include "net/socket.h"      //last always
 
 // ----------------------
 
 // Last:
 // Device manager.
-#include "k3/dev/devmgr.h"  
+#include "devmgr.h"
 
 //
 // + (8) File system.
@@ -282,13 +283,13 @@ Hallelujah, my King - (Transfiguration)
 // Depois de devices.
 
 // fs
-#include "k2/fs/path.h"      // path.
-#include "k2/fs/fatlib.h"    // fat16 library.
-#include "k2/fs/fat.h"       // fat16.
-#include "k2/fs/inode.h"
-#include "k2/fs/exec_elf.h"
-#include "k2/fs/pipe.h"
-#include "k2/fs/fs.h"
+#include "path.h"      // path.
+#include "fatlib.h"    // fat16 library.
+#include "fat.h"       // fat16.
+#include "inode.h"
+#include "exec_elf.h"
+#include "pipe.h"
+#include "fs.h"
 
 
 //
@@ -300,23 +301,23 @@ Hallelujah, my King - (Transfiguration)
 //
 
 // ps
-#include "k2/intake/prio.h"     // Priority
-#include "k2/intake/quantum.h"  // Quantum
-#include "k2/intake/image.h"
-#include "k2/intake/x64cont.h"
-#include "k2/intake/ts.h"
-#include "k2/intake/tasks.h"
-#include "k2/intake/queue.h"
-#include "k2/intake/mk.h"
-#include "k2/intake/dispatch.h"
-#include "k2/intake/msg.h"
-#include "k2/intake/thread.h"
-#include "k2/intake/process.h"
-#include "k2/intake/sched.h"
-#include "k2/intake/schedq.h"
+#include "prio.h"     // Priority
+#include "quantum.h"  // Quantum
+#include "image.h"
+#include "x64cont.h"
+#include "ts.h"
+#include "tasks.h"
+#include "queue.h"
+#include "mk.h"
+#include "dispatch.h"
+#include "msg.h"
+#include "thread.h"
+#include "process.h"
+#include "sched.h"
+#include "schedq.h"
 
 // Precisa de todos os componentes de ke/
-#include "k2/ke/ke.h"
+#include "ke.h"
 
 //
 // + (10) User.
@@ -326,34 +327,34 @@ Hallelujah, my King - (Transfiguration)
 // user/
 //
 
-#include "k2/user/user.h"
+#include "user.h"
 
-#include "k2/zcall/overall/reboot.h"
-#include "k2/zcall/overall/mod.h"
+#include "reboot.h"
+#include "mod.h"
 
 //
 // + (11) Kernel-mode callable interfaces.
 //
 
-#include "k2/zcall/zing.h"
-#include "k2/zcall/zz.h"
-#include "k2/zcall/layer.h"
+#include "zing.h"
+#include "zz.h"
+#include "layer.h"
 
 // syscalls. (Called by the interrups 0x80 and 0x82).
-#include "k2/zcall/apis/sys/sci0.h"
-#include "k2/zcall/apis/sys/sci1.h"
-#include "k2/zcall/apis/sys/sci2.h"
-#include "k2/zcall/apis/sys/syscalls.h"
+#include "sci0.h"
+#include "sci1.h"
+#include "sci2.h"
+#include "syscalls.h"
 // zero. (Used during the kernel initialization)
-#include "k2/zcall/apis/zero/zero.h"
+#include "zero.h"
 // newos. (Called by the interrups 0x80 and 0x82).
-#include "k2/zcall/apis/newos/newos.h"
+#include "newos.h"
 
 // Maskable interrupts
-#include "k2/intake/x64mi.h"
+#include "x64mi.h"
 
-#include "k2/cali/product.h"
+#include "product.h"
 
 // Project California
-#include "k2/cali/cali.h"
+#include "cali.h"
 
